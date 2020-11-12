972db77d5259a2f20d15e5e504e9a2cc575b35c9
Junior Campos <junior.campos@welkomtravel.com>
2020-11-11 22:07:54 -0500

    add private routes

--- src/App.js
+++ src/App.js
@@ -11,23 +11,36 @@ import './assets/sass/utilities.scss';
 import "animate.css";
 import "hover.css";
 import Loading from './components/Loading/index';
-import { PizzaProvider } from './context/PizzaContext';
+import { PizzaProvider, usePizza } from './context/PizzaContext';
 
 // Lazy load
 const Login = React.lazy(() => import('./views/Login'));
 const StoreList = React.lazy(() => import('./views/StoreList'));
 const StoreDetail = React.lazy(() => import('./views/StoreDetail'));
 
+const Routes = () => {
+  const { isLoggedin } = usePizza();
+  return (
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <Route exact path="/" component={Login} />
+        {
+          isLoggedin && (
+            <>
+              <Route exact path="/stores" component={StoreList} />
+              <Route exact path="/stores/:storeId" component={StoreDetail} />
+            </>
+          )
+        }
+      </Router>
+    </Suspense>
+  )
+}
+
 function App() {
   return (
     <PizzaProvider>
-      <Suspense fallback={<Loading />}>
-        <Router>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/stores" component={StoreList} />
-          <Route exact path="/stores/:storeId" component={StoreDetail} />
-        </Router>
-      </Suspense>
+      <Routes />
     </PizzaProvider>
   );
 }
